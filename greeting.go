d6249b21cf0b37842f2fb16de3a52b4e54992396
vitosotdihaet <vitostikitos@gmail.com>
2023-02-12 23:01:38 +0300

    Clean up code

--- greeting.go
+++ greeting.go
@@ -3,12 +3,11 @@ package greeting
 import "fmt"
 
 func greet(name string) string {
-	var msg string
-	msg = fmt.Sprint("Hello, %v!", name)
+	var msg string = fmt.Sprintf("Hello, %v!", name)
 	return msg
 }
 
 func farewell(name string) string {
-	msg := fmt.Sprint("Goodbye, %v!", name)
+	msg := fmt.Sprintf("Goodbye, %v!", name)
 	return msg
 }
